c619e1e7cc02e0d8b31c107ec2da223532b4ada3
fumubyeyi <iragaba.umubyeyi@gmail.com>
2023-11-13 12:03:52 -0500

    parse query parameter to number

--- src/ProductARView/ProductARView.js
+++ src/ProductARView/ProductARView.js
@@ -4,12 +4,12 @@ import { useParams} from "react-router-dom";
 const ProductARView = ({products}) => {
     const { id } = useParams(); // Get the product ID from the URL
   // Find the product with the matching ID
-  const product = products.find((item) => item.pid === id); 
+  const product = products.find((item) => item.pid === Number(id)); 
   console.log("product", product);  
   return (
     <>
         <h1>{`${product.title} AR View`}</h1>
-        <iframe src="https://playcanvas.as/p/NERwz7Rf/" 
+        <iframe src="https://playcanvas.as/e/p/NERwz7Rf" 
         title="AR View"
         style={{width:"80%" ,height:"80vh"}} >
         </iframe>

e7d8299b443da0885578908681ccd5489942ecbf
fumubyeyi <iragaba.umubyeyi@gmail.com>
2023-11-25 22:48:46 -0500

    added textures

--- src/ProductARView/ProductARView.js
+++ src/ProductARView/ProductARView.js
@@ -1,17 +1,70 @@
 import { useState } from "react";
 import { useParams} from "react-router-dom";
+import {sofa_textures, cushionchair_textures} from '../Textures/index';
+
+const PLAYCANVAS_ORIGIN = "https://playcanv.as";
+const PLAYCANVAS_URL = "https://playcanv.as/e/p/NERwz7Rf/";
 
 const ProductARView = ({products}) => {
-    const { id } = useParams(); // Get the product ID from the URL
+  const { id } = useParams(); // Get the product ID from the URL
+
+  const [canvasLoaded, setCanvasLoaded] = useState(false);
+
   // Find the product with the matching ID
   const product = products.find((item) => item.pid === Number(id)); 
-  console.log("product", product);  
+
+  let textures = null;
+
+  switch (product.name){
+    case "CushionChair":
+      textures = cushionchair_textures;
+      break;
+    case "Sofa":
+      textures = sofa_textures;
+      break;
+    default:
+      break;
+  }
+
+  
+
+  window.addEventListener('message', function(event){
+    if (event.origin === PLAYCANVAS_ORIGIN){
+      if(event.data === "app:ready"){
+        setCanvasLoaded(true);
+        
+      }
+    }
+  });
+
+  if (canvasLoaded){
+    loadModelEventHandler(product);
+  }
+
+  function loadModelEventHandler(product){
+    let iframe = document.getElementById("playcanvasIframe"); 
+    console.error(iframe.contentWindow);
+    iframe.contentWindow.postMessage(
+      {
+        name:"LOAD_MODEL", 
+        options : 
+          { modelName : product.name, 
+            position: product.position,
+            rotation: product.rotation,
+            scale: product.scale, 
+            colors: product.colors,
+            textures: textures
+          }
+      }, PLAYCANVAS_ORIGIN);
+    };
+
   return (
     <>
         <h1>{`${product.title} AR View`}</h1>
-        <iframe src="https://playcanvas.as/e/p/NERwz7Rf" 
+        <iframe src={PLAYCANVAS_URL} 
+        id="playcanvasIframe"
         title="AR View"
-        style={{width:"80%" ,height:"80vh"}} >
+        style={{width:"100%" ,height:"80vh"}} >
         </iframe>
     </>
     )

c1810292029706c3ed64569bcc84e183d96b268f
RajeswariYanamaddi <rajeswariyanamaddi@gmail.com>
2023-11-30 18:46:20 -0500

    added logo in app header

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,61 +1,68 @@
 import React from "react";
 import { Link } from "react-scroll";
 import "./Navbar.css";
-
+import logo from '../Images/logo.png'
 class Navbar extends React.Component {
-  state = {  } 
-  render() { 
+  state = {};
+  render() {
     return (
-          <header>
-            <nav
-              className="navbar navbar-expand-lg navbar-white"
-              style={{
-                backgroundColor: "rgb(14, 8, 8)",
-                position: "relative",
-                top: "0",
-              }}
-              id="navbarToggleExternalContent"
+      <header>
+        <nav
+          im
+          className="navbar navbar-expand-lg navbar-white"
+          style={{
+            backgroundColor: "rgb(14, 8, 8)",
+            position: "relative",
+            top: "0",
+          }}
+          id="navbarToggleExternalContent"
+        >
+          <div className="container-fluid ml-0">
+            <a className="navbar-brand" href="#navbarToggleExternalContent">
+              <img src={logo} width="40px" height="40px" alt="..." />
+            </a>
+            <div
+              className="collapse navbar-collapse navbar-brand"
+              id="navbarSupportedContent"
             >
-              <div className="container-fluid ml-0">
-                <a className="navbar-brand" href="#navbarToggleExternalContent">
-                  <img src="..." width="40px" height="40px" alt="..." />
-                </a>
-                <div className="collapse navbar-collapse navbar-brand" id="navbarSupportedContent">
-                  <ul className="navbar-nav me-auto">
-                    <li className="nav-item">
-                      <span className="nav-link">
-                        <Link spy={true} smooth={true} to="products">
+              <ul className="navbar-nav me-auto">
+                <li className="nav-item">
+                  <span className="nav-link">
+                  <Link spy={true} smooth={true} to="products">
                         Our Products
                         </Link>
-                      </span>
-                    </li>
-                    <li className="nav-item">
-                      <span className="nav-link">
-                        <Link spy={true} smooth={true} to="contact">
-                        FAQ
-                        </Link>
-                      </span>
-                    </li>
-                    <li className="nav-item">
-                      <span className="nav-link">
-                        <Link spy={true} smooth={true} to="contact">
-                          About Us
-                        </Link>
-                      </span>
-                    </li>
-                    <li className="nav-item">
-                      <span className="nav-link">
-                        <Link spy={true} smooth={true} to="contact">
-                          Contact Us
-                        </Link>
-                      </span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav> <br />
-          </header>);
+
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <Link spy={true} smooth={true} to="contact">
+                      FAQ
+                    </Link>
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <Link spy={true} smooth={true} to="contact">
+                      About Us
+                    </Link>
+                  </span>
+                </li>
+                <li className="nav-item">
+                  <span className="nav-link">
+                    <Link spy={true} smooth={true} to="contact">
+                      Contact Us
+                    </Link>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>{" "}
+        <br />
+      </header>
+    );
   }
 }
- 
+
 export default Navbar;
